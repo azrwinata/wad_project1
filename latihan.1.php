74c90796de04c2e800399e2975c809dc52baa09d
azrwinata <116279788+azrwinata@users.noreply.github.com>
2023-10-20 13:04:54 +0700

    Add files via upload

---
+++ latihan.1.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Latihan Web 1</title>
+    </head>
+    <body>
+        <h1>Selamat Datang</h1>
+        <label for="input1"> Input 1:</label>
+        <input type="text" id="input1">
+        <br>
+        <label for="input2"> Input 2:</label>
+        <select name="input2" id="input2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+        <br>
+        <input type="submit">
+    </body>
+</html>
